a1e909e90b46bb30f44d69e03bc90ff5af609cfa
diegodataops <diegodataops@gmail.com>
2024-06-03 04:25:06 -0300

    18. Working with Multiple Elements

--- Master_Selenium_Webdriver_with_Python_using_SeleniumBase/test_home.py
+++ Master_Selenium_Webdriver_with_Python_using_SeleniumBase/test_home.py
@@ -41,3 +41,16 @@ class TestHomePage(BaseCase):
 
         # assert the text is visible
         self.assert_text_visible('Showing Results for Lenovo')
+
+
+    def test_nav_links(self):
+    # ^ para rodar apenas uma funcao ->>> pytest -s test_home.py::TestHomePage::test_nav_links
+        self.open('https://practice-react.sdetunicorns.com/')
+        self.assert_text(text='Products', selector='.main-menu li:nth-child(2)')
+
+        expected_nav_test = ['Home', 'Products', 'About Us', 'Contact', 'Upload']
+
+        for i, text in enumerate(expected_nav_test, start=1):
+            # print(i, text)
+            self.assert_text(text, selector=f'.main-menu li:nth-child({i})')
+
